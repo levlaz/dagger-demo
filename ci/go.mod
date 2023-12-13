5324f6735b0125cee12c9da1cbd5542d0e57b62b
Lev Lazinskiy <lev@levlaz.org>
2023-12-12 20:08:56 -0800

    use launchdarkly as module (#2)
    * use launchdarkly as module

--- ci/go.mod
+++ ci/go.mod
@@ -1,16 +1,15 @@
 module ci
 
-go 1.21.5
-
-require dagger.io/dagger v0.9.4
+go 1.21
 
 require (
-	github.com/99designs/gqlgen v0.17.31 // indirect
-	github.com/Khan/genqlient v0.6.0 // indirect
-	github.com/adrg/xdg v0.4.0 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/99designs/gqlgen v0.17.31
+	github.com/Khan/genqlient v0.6.0
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	golang.org/x/sync v0.4.0
+)
+
+require (
+	github.com/stretchr/testify v1.8.3 // indirect
 	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
-	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
-	golang.org/x/sync v0.4.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
 )
--- ci/main.go
+++ ci/main.go
@@ -2,32 +2,47 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
-
-	"dagger.io/dagger"
 )
 
-func main() {
-	ctx := context.Background()
+const (
+	PROJECT = "demo"
+	REPO    = "demo"
+)
 
-	// init dagger client
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+type Demo struct{}
+
+// TODO - fix this part too
+// Lint Backend Go Code
+// func (d *Demo) Lint(ctx context.Context, dir *Directory) (string, error) {
+// 	lintResult, err := dag.
+// 		Golang().
+// 		WithProject(dir).
+// 		GolangciLint(ctx)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return lintResult, nil
+// }
+
+func (d *Demo) Ci(
+	ctx context.Context,
+	dir *Directory,
+	token *Secret,
+) (string, error) {
+	// Lint
+	// TODO - fix this part
+	// out, err := d.Lint(ctx, dir)
+	// if err != nil {
+	// 	return "", err
+	// }
+	var out string
+
+	// Scan for Code References
+	ldOut, err := dag.Launchdarkly().Find(ctx, token, dir, PROJECT, REPO)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	defer client.Close()
+	out = out + "\n" + ldOut
 
-	// use golang container, get version, execute
-	golang := client.Container().
-		From("golang:latest").
-		WithExec([]string{"go", "version"})
-
-	version, err := golang.Stdout(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	// print output
-	fmt.Println("Hello from Dagger and " + version)
+	return out, nil
 }
